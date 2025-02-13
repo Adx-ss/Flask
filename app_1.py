6e49e6386190d4d4f5718d838ad7eab89a4fb41c
Adx-ss <siddharth.sabharwal@adsparkx.com>
2025-02-13 18:50:38 +0530

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -44,4 +44,5 @@ def query_mongo_data():
 
 # ✅ Ensure Gunicorn can find "app"
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
